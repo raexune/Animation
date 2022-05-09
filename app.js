d380d5fe0be529bce01eb37b6c8054d7ec8b31ab
Ramona Fuchs <65505215+raexune@users.noreply.github.com>
2022-05-09 23:27:52 +0200

    media query

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const hero = document.querySelector(".hero");
 const slider = document.querySelector(".slider");
-const logo = document.querySelector(".logo");
-const menu = document.querySelector(".menu");
+const logo = document.querySelector(".nav-logo");
 const headline = document.querySelector(".headline");
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const navList = document.querySelector(".nav-list");
+const menuItem = document.querySelector(".menu-item");
+const navBurger = document.querySelector(".nav-burger");
 
+
+
+//Hero Animation
 const tl = new TimelineMax();
 
 tl.fromTo(hero, 1, {
@@ -17,3 +24,21 @@ tl.fromTo(hero, 1, {
 }, {
     width: "80%"
 });
+
+//Toggle Menu
+
+function toggleMenu() {
+    if (navList.classList.contains("show-menu")) {
+        navList.classList.remove("show-menu");
+        closeMenu.style.display = "none";
+        openMenu.style.display = "block";
+    } else {
+        navList.classList.add("show-menu");
+        closeMenu.style.display = "block";
+        openMenu.style.display = "none";
+    }
+}
+
+
+
+navBurger.addEventListener("click", toggleMenu);
